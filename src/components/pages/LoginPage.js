b399e545db2d9ccfa2239a1ccc6b796112668f00
Prawee Wongsa <prawee.w@integra8t.com>
2018-02-11 16:31:22 +0700

    create login page component

---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import LoginForm from '../forms/LoginForm';
+
+// const LoginPage = () => <Text>Login Page</Text>;
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <LoginForm />
+        );
+    }
+}
+
+export default LoginPage;
